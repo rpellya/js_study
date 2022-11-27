ce2d5867b484c2411e1eb26eb78d9e565ed88f88
rpellya <rpellya@gmail.com>
2022-11-27 23:39:07 +0300

    hello world

--- js-start/js/script.js
+++ js-start/js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+let favproc = "пепперони";
+let secondfavproc = "сосиски";
+let nicepizza = favproc + " и " + secondfavproc;
+console.log(nicepizza);
+
+var total = 6+7+8;
+console.log(total);
+
+console.log((8*3)/6);
+
+var agebro = 11;
+var agesist = 13; 
+var amountbroorsist = 2;
+var averageage = (agebro+agesist)/amountbroorsist;
+console.log(averageage);
+
+let number = 5;
+let answer = (20 / number) + ((8 * number) / (6-2));
+console.log(answer);
+
+console.log(6 + " футов " + 7 + " футов ");
+
+console.log("Работаю с " + 9 + " до " + 5);
+
+console.log(7 + "3");
+
+console.log(10 / 0);
