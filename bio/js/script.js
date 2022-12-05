ef57384c02dbc825fb9b23340bd7732091b9465a
rpellya <rpellya@gmail.com>
2022-12-05 18:46:31 +0300

    Закончил 3 главу. Написал биографию о себе на JS

---
+++ bio/js/script.js
@@ -0,0 +1,12 @@
+let name = "Роман";
+let surname = "Пелля";
+let secondname = "Юрьевич";
+let hobby = "программированием на JavaScript";
+let age = 18;
+let Favmovie = "Интерстеллар";
+
+let полноеИмя = surname + ' ' + name + ' ' + secondname;
+
+let bio = 'Меня зовут ' + полноеИмя + ', мне ' + age + ' лет.' + ' Занимаюсь ' + hobby + ' и мой любимый фильм - ' + Favmovie;
+
+console.log(bio);
