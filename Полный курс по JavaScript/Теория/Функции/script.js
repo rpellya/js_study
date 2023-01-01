76a6a7fe89513e627eb855e992111d9db28c507b
rpellya <rpellya@gmail.com>
2023-01-01 23:27:52 +0300

    прошел 2 темы в Раздале Функции

--- Полный курс по JavaScript/Теория/Функции/script.js
+++ Полный курс по JavaScript/Теория/Функции/script.js
@@ -27,5 +27,67 @@ sum(a,b) // 20
  * 
  * Функция - это объект
  * 
- * 
  */
+
+function myFn(a, b){
+   let c 
+   a += 1
+   c = a + b
+   return c
+}
+
+myFn(10,3) // 14
+
+/**
+ * myFn - имя 
+ * (a,b) - параметры, названия перменных при объевление функции
+ * {...} - тело функции
+ * return - результат (после слово return функция свою работу прекратит)
+ * 
+ * Если нет инструкции return, то функция возвращает undefined(не определено)
+ * 
+ * Вызов функции
+ * myfn() - вызов функции
+ * (10,3) - аргументы - те значения, которые передаем в вызове функции
+ */
+
+
+//ПЕРЕДАЧА ЗНАЧЕНИЯ ПО ССЫЛКЕ
+
+const personOne = {
+   name: 'Bob',
+   age: 21
+}
+
+function increasePersonAge(person){
+   person.age += 1
+   return person
+}
+
+increasePersonAge(personOne) // Передаче объекта по ссылке
+console.log(personOne.age) // 22
+
+/**
+ * Внутри функции НЕ РЕКОМЕНДУЕТСЯ мутировать ВНЕШНИЕ объекты
+ */
+
+//Создание копии объекта
+
+const personOne2 = {
+   name: 'Bob',
+   age: 21
+}
+
+function increasePersonAge2(person){
+   const updatePerson2 = Object.assign({}, person)
+   updatePerson2.age += 1
+   return updatePerson2
+}
+
+const updatePersonOne2 = increasePersonAge2(personOne2) 
+console.log(personOne2.age) // 21
+console.log(updatePersonOne2.age) // 22
+
+
+//КОЛБЭК ФУНКЦИИ
+
