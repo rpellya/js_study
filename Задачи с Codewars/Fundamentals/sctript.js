28920197a64db3f836bc666b8ffb290cfbabb6e2
rpellya <rpellya@gmail.com>
2023-01-06 00:33:26 +0300

    Решил 1 задачу с Codewars

--- Задачи с Codewars/Fundamentals/sctript.js
+++ Задачи с Codewars/Fundamentals/sctript.js
@@ -1,4 +1,5 @@
 /*
+1.
 ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 If the function is passed a valid PIN string, return true, else return false. 
  */
@@ -27,3 +28,28 @@ function validatePIN(pin) {
 }
 
 */
+
+/*
+2. This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+
+const accum = s =>{
+   const result = [];
+   const lowerStr = s.toLowerCase();
+   for(let i = 0; i<s.length; i++){
+      let str = lowerStr[i].toUpperCase();
+      for(let j = 0; j < i; j++){
+         str+=lowerStr[i];
+      }
+      result.push(str);
+   }
+   return result.join('-');
+}
