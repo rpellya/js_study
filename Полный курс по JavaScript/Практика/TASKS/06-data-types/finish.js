12b8ebc73236c8d37b80ad76bd033c078c6dd2a3
rpellya <rpellya@gmail.com>
2023-01-12 20:21:45 +0300

    Сделал 5 задание

--- Полный курс по JavaScript/Практика/TASKS/06-data-types/finish.js
+++ Полный курс по JavaScript/Практика/TASKS/06-data-types/finish.js
@@ -12,3 +12,34 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const string = 'abc'
+
+const numb = 11 
+
+const boolean = true
+
+const nullVariable = null
+
+const undefinedVariable = undefined 
+
+const object = {
+   name: 'Roman',
+   pupil: true
+} 
+
+const array = [1,2,3]
+
+console.log(typeof string) // string
+
+console.log(typeof numb) // number
+
+console.log(typeof boolean) // boolean
+
+console.log(typeof nullVariable) // object
+
+console.log(typeof undefinedVariable) // undefined
+
+console.log(typeof object) // object
+
+console.log(typeof array) // object
